3ad0cb83dfd30e5c0715eb00c1afa3355ca30012
Gabe Cook <gabe565@gmail.com>
2024-08-30 02:16:04 -0500

    test: Add more test cases

--- pkg/ansi2txt/writer_test.go
+++ pkg/ansi2txt/writer_test.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/fatih/color"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -17,11 +16,20 @@ func TestWriter_Write(t *testing.T) {
 		wantErr require.ErrorAssertionFunc
 	}{
 		{"no escape", "abc", "abc", require.NoError},
-		{"blue", color.BlueString("blue"), "blue", require.NoError},
-		{"formatted", color.New(color.Bold, color.Italic).Sprint("formatted"), "formatted", require.NoError},
+		{"color", "\x1b[34mblue", "blue", require.NoError},
+		{"multiple", "\x1b[1mbold\x1b[0m and \x1b[4munderlined\x1b[0m", "bold and underlined", require.NoError},
+		{"nested", "\x1b[31;1mred bold\x1b[0m normal", "red bold normal", require.NoError},
+		{"clear screen", "\x1b[2Jclear", "clear", require.NoError},
 		{"move cursor", "\x1b[10;10Hmove", "move", require.NoError},
 		{"bell", "a\x07bc", "abc", require.NoError},
 		{"only escape", "a\x1bbc", "abc", require.NoError},
+		{"title", "\x1b]0;title\x07abc", "abc", require.NoError},
+		{"character set", "\x1b%Gabc", "abc", require.NoError},
+		{"hide cursor", "\x1b[?25lhidden\x1b[?25h", "hidden", require.NoError},
+		{"invalid escape sequence", "a\x1b[999mbc", "abc", require.NoError},
+		{"title terminated by escape", "\x1b]2;title\x1b\"abc", "abc", require.NoError},
+		{"sequences mixed with text", "normal \x1b[1mbold\x1b[0m normal \x1b[4munderlined\x1b[0m", "normal bold normal underlined", require.NoError},
+		{"unclosed sequence", "text \x1b[31 no close", "text no close", require.NoError},
 		{
 			"complex",
 			"foo\x1b[1mbar\x1b[0m\n" +
